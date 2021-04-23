3e314001bfe3b96b17b8ca343597964c8423d7de
Leonardo Aparecido <contato.leonardoap@gmail.com>
2021-04-23 01:11:22 -0300

    basico do cambio

--- src/app/home/cambio/cambio.page.ts
+++ src/app/home/cambio/cambio.page.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+type Moedas = 'Real' | 'Dolar';
 
 @Component({
   selector: 'app-cambio',
   templateUrl: './cambio.page.html',
   styleUrls: ['./cambio.page.scss'],
 })
-export class CambioPage implements OnInit {
+
+export class CambioPage {
 
   constructor() { }
 
-  ngOnInit() {
+  public moedaEnt: number;
+  public tipoMoedaEnt: Moedas;
+  public moedaSaida: string = "10";
+  public tipoMoedaSaida: Moedas;
+
+  private unidadesMoedas = {
+    Real: 1,
+    Dolar: 5.5,
+  };
+
+  public calcCambio() {
+    const token = this.moedaEnt * this.unidadesMoedas[this.tipoMoedaEnt];
+    const tr = token / this.unidadesMoedas[this.tipoMoedaSaida];
+    this.moedaSaida = tr.toString();
   }
 
 }
