74189105dbdcb6f730b7f413870b2381e8de71bd
Leonardo Aparecido <contato.leonardoap@gmail.com>
2021-04-25 01:22:15 -0300

    simulacao lista

--- src/app/home/simulacao/simulacao.page.ts
+++ src/app/home/simulacao/simulacao.page.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
+interface Simulacao{
+  nome: String;
+  percentual: number;
+}
 @Component({
   selector: 'app-simulacao',
   templateUrl: './simulacao.page.html',
   styleUrls: ['./simulacao.page.scss'],
 })
-export class SimulacaoPage implements OnInit {
+
+export class SimulacaoPage{
+
+  public simulacoes : Simulacao[] = [];
+
+  public newInvestName = "";
+  public newPercentYear = 0.0;
 
   constructor() { }
 
-  ngOnInit() {
+  public addNewSimulacao(){
+    if (this.newInvestName.trim().length == 0) {
+      return;
+    }
+    if (this.newPercentYear <= 0) {
+      return;
+    }
+
+    this.simulacoes.push({
+      nome: this.newInvestName.trim(),
+      percentual: this.newPercentYear
+    })
+
+    this.newInvestName = '';
+    this.newPercentYear = 0.0;
   }
 
+  public removeSimulacao(toRemove: Simulacao) {
+    const index = this.simulacoes.indexOf(toRemove)
+    this.simulacoes.splice(index, 1);
+  }
 }
