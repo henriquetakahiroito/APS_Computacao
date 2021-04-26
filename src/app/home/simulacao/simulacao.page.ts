31407773d03f57539743dfa946a810255e6d8cd6
Leonardo Aparecido <contato.leonardoap@gmail.com>
2021-04-26 01:43:33 -0300

    calculo de simulacao

--- src/app/home/simulacao/simulacao.page.ts
+++ src/app/home/simulacao/simulacao.page.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {AlertController} from '@ionic/angular';
 
 interface Simulacao{
   nome: String;
@@ -14,10 +15,14 @@ export class SimulacaoPage{
 
   public simulacoes : Simulacao[] = [];
 
+  public newValor = 0.0;
+  public newTempo = 0.0;
   public newInvestName = "";
   public newPercentYear = 0.0;
 
-  constructor() { }
+  constructor(
+    private alertController: AlertController
+  ) {  }
 
   public addNewSimulacao(){
     if (this.newInvestName.trim().length == 0) {
@@ -40,4 +45,31 @@ export class SimulacaoPage{
     const index = this.simulacoes.indexOf(toRemove)
     this.simulacoes.splice(index, 1);
   }
+
+  public calculateSimulacao(){
+    let text = '';
+    let valorTemp = 0;
+
+    for(let s of this.simulacoes){
+      valorTemp = this.newValor * Math.pow((1+s.percentual), (this.newTempo/12));
+      text += s.nome + " - " + valorTemp.toFixed(3) + "&emsp;";
+    }
+        
+    return text;
+  }
+
+  public async Simulacao() {
+    if (this.newValor <= 0 || this.newTempo <= 0) {
+      return;
+    }
+    if(this.simulacoes.length <= 0)
+      return;
+
+    const alert = await this.alertController.create({
+      header: 'Resultado da simulação é',
+      message: this.calculateSimulacao(),
+    });
+    alert.present();
+  }
+
 }
